5781e96604c0964d48456aa8e66745292a9f8b53
Mohammad Alian <malian2@illinois.edu>
2017-05-25 22:38:06 -0500

    apache-bench: added dist-gem5 scripts

---
+++ apache-bench/run-scripts/dist-gem5/ckpt.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Author Mohammad Alian <malian2@illinois.edu>
+# Sample ckpt script for dist-gem5 to run apache-bench
+#
+# Please check http://publish.illinois.edu/icsl-pdgem5/benchmarks/
+# to prepare the disk-image
+
+fsconfig=$M5_PATH/configs/example/arm/dist_bigLITTLE.py
+swconfig=$M5_PATH/configs/example/arm/dist_bigLITTLE.py
+m5exe=$M5_PATH/build/ARM/gem5.opt
+ckptdir=$(pwd)/ckptdir/multi-node-ab
+rundir=$ckptdir
+diskimage=$M5_PATH/disks/aarch64-ubuntu-trusty-headless.img
+kernel=$M5_PATH/binaries/vmlinux.vexpress_gem5_v1_64.20170426
+dtb=$M5_PATH/binaries/armv8_gem5_v1_4cpu.20170426.dtb
+script=$(pwd)/boot-ab.rcS
+nnodes=4
+debugflags="--debug-flags=DistEthernet"
+
+$M5_PATH/util/dist/gem5-dist.sh -n $nnodes \
+                                -x $m5exe \
+                                -s $swconfig \
+                                -f $fsconfig \
+                                -r $rundir \
+                                -c $ckptdir \
+                                --m5-args \
+                                    $debugflags \
+                                --fs-args \
+                                    --cpu-type=atomic \
+                                    --big-cpus=4 \
+                                    --little-cpus=0 \
+                                    --disk=$diskimage \
+                                    --kernel=$kernel \
+                                    --dtb=$dtb \
+                                    --bootscript=$script \
+                                --cf-args \
+                                    --dist-sync-start=1000000000000t
